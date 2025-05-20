6442626343ca0f1faa95d5f6df08bd6d50478a21
Nurlanbek Fazliddin <iamnurlanbek@gmail.com>
2025-05-20 07:23:11 +0500

    Added lesson-9

--- lesson-9/homework/homework9.py
+++ lesson-9/homework/homework9.py
@@ -1,6 +1,6 @@
-# # Homework:
+"""# Homework:
 
-# # Object-Oriented Programming (OOP) Exercises
+# Object-Oriented Programming (OOP) Exercises"""
 
 """ 1. Circle Class
  Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter."""
