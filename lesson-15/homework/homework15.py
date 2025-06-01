6ba11cad8d856d8ff9b72d3b596c2f9bc326e7be
Nurlanbek Fazliddin <iamnurlanbek@gmail.com>
2025-06-01 20:10:15 +0500

    Homework 15

--- lesson-15/homework/homework15.py
+++ lesson-15/homework/homework15.py
@@ -1,19 +1,16 @@
 """Create a new database with a table named Roster that has three fields: Name, Species, and Age. 
 The Name and Species columns should be text fields, and the Age column should be an integer field.
-
 Populate your new table with the following values:
-
 Name	Species	Age
 Benjamin Sisko	Human	40
 Jadzia Dax	Trill	300
 Kira Nerys	Bajoran	29
 Update the Name of Jadzia Dax to be Ezri Dax
-
 Display the Name and Age of everyone in the table classified as Bajoran."""
 
 import sqlite3
 
-connection = sqlite3.connect(r"C:\Python\lesson-15\mydb.db")
+connection = sqlite3.connect(r"C:\Python\lesson-15\homework\mydb.db")
 my_cursor = connection.cursor()
 
 # Creating table
